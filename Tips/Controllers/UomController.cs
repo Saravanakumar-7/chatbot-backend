5b0ee1389c6f6dcd2c955dd469dc2086f6f32219
Pramanayagam <pramanayagam@wyzmindz.com>
2022-10-21 15:57:22 +0530

    Created Uom controller

---
+++ Tips/Controllers/UomController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tips.Services;
+
+namespace Tips.Controllers
+{
+    public class UomController
+    {
+        private readonly IUomService _uomService;
+
+
+        public UomController(IUomService uomService)
+        {
+            _uomService = uomService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUom()
+        {
+            var uomDetails = await _uomService.GetAllUomDetails();
+            return null; //Ok(uomDetails);   
+        }
+    }
+}
