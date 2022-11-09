ef89618838c66f540f647741fd4c1dc90cef0b17
Pramanayagam <pramanayagam@wyzmindz.com>
2022-11-09 13:54:46 +0530

    Added Repository base files

---
+++ Tips.SalesService.Api/Contracts/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips.SalesService.Api.Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        Task<T> Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
