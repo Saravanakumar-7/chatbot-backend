6abb53824d2cab3938c80d316afdf3cc155b0dcc
Aravindch123 <90240320+Aravindch123@users.noreply.github.com>
2022-11-16 15:33:15 +0530

    Created PFpurchase and Grin partially

---
+++ Tips.SalesService.Api/Entities/Grin.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips.SalesService.Api.Entities
+{
+    public class Grin
+    {
+        public int Id { get; set; }
+
+        public string? VendorName { get; set; }
+
+        public string? VendorId { get; set; }
+
+        public string? InvoiceNumber { get; set; }
+
+        public string? InvoiceValue { get; set; }
+
+        public string? PoNumber { get; set; }
+
+        public DateTime? InvoiceDate { get; set; }
+
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+
+        public List<GrinParts>? GrinParts { get; set; }
+
+    }
+}
---
+++ Tips.SalesService.Api/Entities/GrinParts.cs
@@ -0,0 +1,58 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips.SalesService.Api.Entities
+{
+    public class GrinParts
+    {
+        public int Id { get; set; }
+
+        public string ItemNODescription { get; set; }
+
+        public string MftrItemNumber { get; set; }
+
+        public string ProjectNumber { get; set; }
+
+        public string ManufactureBatchNumber { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int POOrderQuantity { get; set; }
+
+        public int POBalancedQuantity { get; set; }
+
+        public decimal POUnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string UOM { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+        public DateTime? ManufactureDate { get; set; }
+
+        public string COCUpload { get; set; }
+        public decimal? SGST { get; set; }
+
+        public decimal? IGST { get; set; }
+
+        public decimal? CGST { get; set; }
+
+        public decimal? UTGST { get; set; }
+
+
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+
+        public int GrinId { get; set; }
+
+        public Grin? Grin { get; set; }
+    }
+}
