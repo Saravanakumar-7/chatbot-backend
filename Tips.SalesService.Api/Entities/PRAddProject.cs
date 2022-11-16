ff7990631155f6abd756b5ef4531c5d53a2542f0
Aravindch123 <90240320+Aravindch123@users.noreply.github.com>
2022-11-16 16:51:23 +0530

    Created PR partially

--- Tips.SalesService.Api/Entities/PRAddProject.cs
+++ Tips.SalesService.Api/Entities/PRAddProject.cs
@@ -9,11 +9,10 @@ using System.Threading.Tasks;
 
 namespace Tips.SalesService.Api.Entities
 {
-    public class Addproject
+    public class PRAddProject
     {
         public int Id { get; set; }
         public string ProjectNumber { get; set; }
         public string ProjectQuantity { get; set; }
-
     }
 }
