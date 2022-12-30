293a8ad60e316e190a4205525c2edc08fcc7fd6b
Pavankumar-Boya <116349561+Pavankumar-Boya@users.noreply.github.com>
2022-12-30 15:59:09 +0530

    done grin partially

---
+++ Tips.Grin.Api/Entities/ProjectNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace Tips.Grin.Api.Entities
+{
+    public class ProjectNumber
+    {
+        public int Id { get; set; }
+        public string? Projectnumber { get; set; }
+        public string? ItemNumber { get; set; }
+        public decimal? Quantity { get; set; }
+        public decimal? ProjectQuantity { get; set; }
+
+        public string Unit { get; set; }
+
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+
+        public int ProjectNumberId { get; set; }
+        public GrinParts? GrinParts { get; set; }
+
+
+
+
+
+    }
+}
