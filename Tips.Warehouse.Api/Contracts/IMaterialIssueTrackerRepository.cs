0dc055208fe9a1affde81a7daa256ddaaf421190
Pramanayagam <Pramanayagam@wyzmindz.com>
2023-07-29 17:43:27 +0530

    Created MAterial Issue Tracker

---
+++ Tips.Warehouse.Api/Contracts/IMaterialIssueTrackerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tips.Warehouse.Api.Entities;
+
+namespace Tips.Warehouse.Api.Contracts
+{
+    public interface IMaterialIssueTrackerRepository : IRepositoryBase<MaterialIssueTracker>
+    {
+        Task<int> AddDataToMaterialIssueTracker(MaterialIssueTracker);
+    }
+}
---
+++ Tips.Warehouse.Api/Repository/MaterialIssueTrackerRepository.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System.Linq.Expressions;
+using Tips.Warehouse.Api.Contracts;
+using Tips.Warehouse.Api.Entities;
+
+namespace Tips.Warehouse.Api.Repository
+{
+    public class MaterialIssueTrackerRepository : RepositoryBase<MaterialIssueTracker>,IMaterialIssueTrackerRepository
+    {
+        private readonly string _connectionString;
+        private readonly MySqlConnection _connection;
+        public MaterialIssueTrackerRepository(TipsWarehouseDbContext repositoryContext, MySqlConnection connection) : base(repositoryContext)
+        {
+            _connection = connection;
+        }
+
+        public async Task<int> AddDataToMaterialIssueTracker(MaterialIssueTracker shopOrderMaterialIssue)
+        {
+            shopOrderMaterialIssue.CreatedBy = "Admin";
+            shopOrderMaterialIssue.CreatedOn = DateTime.Now;
+            shopOrderMaterialIssue.Unit = "Bangalore";
+            var result = await Create(shopOrderMaterialIssue);
+
+            return result.Id;
+        }
+    }
+}
