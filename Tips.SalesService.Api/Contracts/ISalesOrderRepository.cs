2b1f4b5cc997d845a99949c18dcb00c87388665a
Aravindch123 <90240320+Aravindch123@users.noreply.github.com>
2023-11-10 10:09:29 +0530

    sales order changes

--- Tips.SalesService.Api/Contracts/ISalesOrderRepository.cs
+++ Tips.SalesService.Api/Contracts/ISalesOrderRepository.cs
@@ -24,11 +24,10 @@ namespace Tips.SalesService.Api.Contracts
         Task<PagedList<SalesOrder>> GetAllSalesOrderForecast(PagingParameter pagingParameter, SearchParammes searchParammes);
         Task<PagedList<SalesOrder>> GetAllSalesOrderRfq(PagingParameter pagingParameter, SearchParammes searchParammes);
         Task<IEnumerable<SalesOrderIdNameListDto>> GetAllSalesOrderIdNameList();
+         
+        //Task<List<ProjectSODetailDto>> GetProjectDetailsByItemNo(string itemNumber, string projectType);
+        Task<List<ProjectSODetailDto>> GetProjectDetailsByItemNo(string itemNumber);
 
-        //Task<string> UpdateSOBasedOnCreatingDO();
-
-        //Task<string> UpdateSOBasedOnCreatingShopOrder();
-        Task<List<ProjectSODetailDto>> GetProjectDetailsByItemNo(string itemNumber, string projectType);
         Task<List<ProjectSOSADetailDto>> GetProjectDetailsBySAItemNo(string fgItemNumbers);
         Task<List<SalesOrderQtyDto>> GetSalesOrderQtyDetailsByItemNo(string itemNumber,string projectNo);
         Task<IEnumerable<SalesOrderIdNameListDto>> GetAllActiveSalesOrderNameList();
