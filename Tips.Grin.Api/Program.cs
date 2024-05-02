b9b0d2252631ab0bf18221315128a951e0e5cbf2
Santhosh <santhosh.ganesa@wyzmindz.com>
2024-05-02 12:14:09 +0530

    changes in grin,programcs file

--- Tips.Grin.Api/Program.cs
+++ Tips.Grin.Api/Program.cs
@@ -60,9 +60,7 @@ builder.Services.AddScoped<IBinningItemsRepository, BinningItemsRepository>();
 builder.Services.AddScoped<IGrinPartsRepository, GrinPartsRepository>();
 builder.Services.AddScoped<IOpenGrinRepository, OpenGrinRepository>();
 builder.Services.AddScoped<IBinningLocationRepository, BinningLocationRepository>();
-builder.Services.AddScoped<IWeightedAvgRateRepository, WeightedAvgRateRepository>();
 builder.Services.AddScoped<IWeightedAvgCostRepository, WeightedAvgCostRepository>();
-
 builder.Services.AddScoped<IIQCConfirmationItemsRepository, IQCConfirmationItemsRepository>();
 //builder.Services.AddScoped<IReturnGrinDocumentUploadRepository, ReturnGrinDocumentUpload>();
 builder.Services.AddControllersWithViews()
