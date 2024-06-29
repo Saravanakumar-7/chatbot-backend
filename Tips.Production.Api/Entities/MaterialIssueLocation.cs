eff8d40da8ea84ceafdc02ec3fd84a5a624bc8d4
Santhosh <santhosh.ganesa@wyzmindz.com>
2024-06-29 13:52:01 +0530

    changes in material Issue and invoice

--- Tips.Production.Api/Entities/MaterialIssueLocation.cs
+++ Tips.Production.Api/Entities/MaterialIssueLocation.cs
@@ -3,9 +3,9 @@
     public class MaterialIssueLocation
     {
         public int Id { get; set; }
-        public string? PartNumber { get; set; }
-        public string? Warehouse { get; set; }
-        public string? Location { get; set; }
+        public string PartNumber { get; set; }
+        public string Warehouse { get; set; }
+        public string Location { get; set; }
         public decimal DistributingQty { get; set; }
         public int MaterialIssueItemId { get; set; }
         public MaterialIssueItem MaterialIssueItem { get; set; }
