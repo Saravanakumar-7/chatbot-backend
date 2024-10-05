9ecb596d9e2f116a17ec2b07abb32a1a274c3769
Hisham9880 <136053286+Hisham9880@users.noreply.github.com>
2024-10-05 17:51:30 +0530

    Changes in GRIN and InventoryTransaction

--- Tips.Grin.Api/Entities/DTOs/GrinDto.cs
+++ Tips.Grin.Api/Entities/DTOs/GrinDto.cs
@@ -268,6 +268,33 @@ namespace Tips.Grin.Api.Entities.DTOs
         public string? ShopOrderNo { get; set; }
 
     }
+    public class grinInventoryTrasactionPostDto
+    {
+        public string? PartNumber { get; set; }
+        public string? LotNumber { get; set; }
+        public string? MftrPartNumber { get; set; }
+        public string? Description { get; set; }
+        public string? ProjectNumber { get; set; }
+        public decimal Issued_Quantity { get; set; }
+        public string? UOM { get; set; }
+        public DateTime Issued_DateTime { get; set; }
+        public string? Issued_By { get; set; }
+        public string? ShopOrderId { get; set; }
+        public string? ReferenceID { get; set; }
+        public string? ReferenceIDFrom { get; set; }
+        public decimal BOM_Version_No { get; set; }
+        public string? From_Location { get; set; }
+        public string? TO_Location { get; set; }
+        public bool ModifiedStatus { get; set; } = false;
+        public string? GrinMaterialType { get; set; } = "Bought Out";
+        public string? Remarks { get; set; }
+        public bool IsStockAvailable { get; set; }
+        public string Warehouse { get; set; }
+        public string? GrinNo { get; set; }
+        public int? GrinPartId { get; set; }
+        public PartType PartType { get; set; }
+        public string? shopOrderNo { get; set; }
+    }
     public class GrinNoForIqcAndBinning
     {
         public string? GrinNumber { get; set; }
