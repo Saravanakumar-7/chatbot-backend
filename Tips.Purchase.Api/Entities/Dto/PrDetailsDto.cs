2ce7a92adfa93c05a38bfb06509404895aa9d543
Hisham9880 <136053286+Hisham9880@users.noreply.github.com>
2025-01-16 16:21:20 +0530

    Chnages in PO

--- Tips.Purchase.Api/Entities/Dto/PrDetailsDto.cs
+++ Tips.Purchase.Api/Entities/Dto/PrDetailsDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string? PRNumber { get; set; }
         public decimal? Qty { get; set; }
+        public bool ToClosePR { get; set; } = false;
         public List<PrDetailDocumentUploadDto>? PrDetailDocumentUploadDtos { get; set; }
     }
     public class PrDetailsPostDto
