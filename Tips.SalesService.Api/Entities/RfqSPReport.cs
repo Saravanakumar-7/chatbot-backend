c33e9019220235e2e96d6c022b86e460c92baf49
shreedharvk45 <shreedharv.kalahal@idamtat.in>
2025-06-18 11:58:18 +0530

    created new api for keus RFQ

--- Tips.SalesService.Api/Entities/RfqSPReport.cs
+++ Tips.SalesService.Api/Entities/RfqSPReport.cs
@@ -16,4 +16,22 @@
         public decimal? Qty { get; set; }
         public string? RoomName { get; set; }
     }
+
+    public class RfqSPReportForKeus
+    {
+        public string? LeadId { get; set; }
+        public string? CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public int? RevisionNumber { get; set; }
+        public string? TypeOfSolution { get; set; }
+        public string? ProductType { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? ItemNumber { get; set; }
+        public string? Description { get; set; }
+        public decimal? Qty { get; set; }
+        public string? RoomName { get; set; }
+    }
 }
