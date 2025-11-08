41926683336ce8d3922ec8e09f315edb902828af
Santhosh G <santhosh.ganesa@wyzmindz.com>
2025-11-08 13:24:10 +0530

    chnages in so

--- Tips.SalesService.Api/Contracts/ISalesOrderRepository.cs
+++ Tips.SalesService.Api/Contracts/ISalesOrderRepository.cs
@@ -73,7 +73,7 @@ namespace Tips.SalesService.Api.Contracts
         Task<IEnumerable<SOMonthlyConsumptionSPReport>> GetSOMonthlyConsumptionSPReportWithParam(string CustomerId);
         Task<IEnumerable<CustomerWiseTransactionSPReport>> GetCustomerWiseTransactionSPReportWithParam(string CustomerId);
         Task<IEnumerable<SOMonthlyConsumptionSPReport>> GetSOMonthlyConsumptionSPReportWithDate(DateTime? FromDate, DateTime? ToDate);
-        Task<IEnumerable<FGSalesOrderSPReport>> GetFGSalesOrderSPReportWithParam(string? SalesOrderNumber, string? ProjectNumber);
+        Task<IEnumerable<FGSalesOrderSPReportWithDate>> GetFGSalesOrderSPReportWithParam(string? SalesOrderNumber, string? ProjectNumber);
         Task<IEnumerable<FGSalesOrderSPReportWithDate>> GetFGSalesOrderSPReportWithDate(DateTime? FromDate, DateTime? ToDate);
         Task<IEnumerable<RfqSalesOrderSPReport>> GetRfqSalesOrderSPReportWithParam(string CustomerName, string SalesOrderNumber, string KPN, string SOStatus,string CustomerId);
         Task<IEnumerable<RfqSalesOrderRoomWiseSPReport>> GetRfqSalesOrderRoomWiseSPReportWithParam(string CustomerName, string SalesOrderNumber, string KPN, string CustomerId);
